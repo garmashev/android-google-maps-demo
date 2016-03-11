f8e09a162adc4eb4c5bc8988275146f6dce93295
max <lituchii.maksim@mail.ru>
2016-03-11 16:00:35 +0200

    Show point on map

--- app/src/main/java/com/example/mapdemo/MapDemoActivity.java
+++ app/src/main/java/com/example/mapdemo/MapDemoActivity.java
@@ -5,9 +5,9 @@ import android.app.Dialog;
 import android.content.Intent;
 import android.content.IntentSender;
 import android.location.Location;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v4.app.DialogFragment;
-import android.support.v4.app.FragmentActivity;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.Toast;
@@ -23,78 +23,116 @@ import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+
+import javax.net.ssl.HttpsURLConnection;
 
 import permissions.dispatcher.NeedsPermission;
 import permissions.dispatcher.RuntimePermissions;
 
 @RuntimePermissions
 public class MapDemoActivity extends AppCompatActivity implements
-		GoogleApiClient.ConnectionCallbacks,
-		GoogleApiClient.OnConnectionFailedListener,
-		LocationListener {
+        GoogleApiClient.ConnectionCallbacks,
+        GoogleApiClient.OnConnectionFailedListener,
+        LocationListener {
 
-	private SupportMapFragment mapFragment;
-	private GoogleMap map;
-	private GoogleApiClient mGoogleApiClient;
-	private LocationRequest mLocationRequest;
-	private long UPDATE_INTERVAL = 60000;  /* 60 secs */
-	private long FASTEST_INTERVAL = 5000; /* 5 secs */
+    private SupportMapFragment mapFragment;
+    private GoogleMap map;
+    private GoogleApiClient mGoogleApiClient;
+    private LocationRequest mLocationRequest;
+    private long UPDATE_INTERVAL = 60000;  /* 60 secs */
+    private long FASTEST_INTERVAL = 5000; /* 5 secs */
 
-	/*
-	 * Define a request code to send to Google Play services This code is
-	 * returned in Activity.onActivityResult
-	 */
-	private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
+    private double latitude ;
+    private double longitude ;
+    private long dateTime ;
+    private String URL = "http://195.140.162.44:8080/spyder/records/+380996276020";
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) { 
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.map_demo_activity);
+    /*
+     * Define a request code to send to Google Play services This code is
+     * returned in Activity.onActivityResult
+     */
+    private final static int CONNECTION_FAILURE_RESOLUTION_REQUEST = 9000;
 
-		mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
-		if (mapFragment != null) {
-			mapFragment.getMapAsync(new OnMapReadyCallback() {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map_demo_activity);
+
+        mapFragment = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map));
+        if (mapFragment != null) {
+            mapFragment.getMapAsync(new OnMapReadyCallback() {
                 @Override
                 public void onMapReady(GoogleMap map) {
                     loadMap(map);
                 }
             });
-		} else {
-			Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
-		}
+        } else {
+            Toast.makeText(this, "Error - Map Fragment was null!!", Toast.LENGTH_SHORT).show();
+        }
 
-	}
+    }
 
     protected void loadMap(GoogleMap googleMap) {
         map = googleMap;
         if (map != null) {
             // Map is ready
+            new RequestTask().execute(URL);
+//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(48.4651708, 35.0467873), 15));
+//
+//            map.addMarker(new MarkerOptions()
+//                    .title("11.03.2016, 10:46")
+//                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher))
+//                    .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+//                    .position(new LatLng(48.4651708, 35.0467873)));
+
+//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(
+//                    new LatLng(-18.142, 178.431), 2));
+//            map.addPolyline(new PolylineOptions().geodesic(true)
+//                            .add(new LatLng(-33.866, 151.195))  // Sydney
+//                            .add(new LatLng(-18.142, 178.431))  // Fiji
+//                            .add(new LatLng(21.291, -157.821))  // Hawaii
+//                            .add(new LatLng(37.423, -122.091))  // Mountain View
+//            );
             Toast.makeText(this, "Map Fragment was loaded properly!", Toast.LENGTH_SHORT).show();
-			MapDemoActivityPermissionsDispatcher.getMyLocationWithCheck(this);
+            MapDemoActivityPermissionsDispatcher.getMyLocationWithCheck(this);
         } else {
             Toast.makeText(this, "Error - Map was null!!", Toast.LENGTH_SHORT).show();
         }
     }
 
-	@Override
-	public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
-		super.onRequestPermissionsResult(requestCode, permissions, grantResults);
-		MapDemoActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
-	}
+    @Override
+    public void onRequestPermissionsResult(int requestCode, String[] permissions, int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        MapDemoActivityPermissionsDispatcher.onRequestPermissionsResult(this, requestCode, grantResults);
+    }
 
-	@NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
-	void getMyLocation() {
-		if (map != null) {
-			// Now that map has loaded, let's get our location!
-			map.setMyLocationEnabled(true);
-			mGoogleApiClient = new GoogleApiClient.Builder(this)
-					.addApi(LocationServices.API)
-					.addConnectionCallbacks(this)
-					.addOnConnectionFailedListener(this).build();
-			connectClient();
-		}
-	}
+    @NeedsPermission({Manifest.permission.ACCESS_FINE_LOCATION, Manifest.permission.ACCESS_COARSE_LOCATION})
+    void getMyLocation() {
+        if (map != null) {
+            // Now that map has loaded, let's get our location!
+            map.setMyLocationEnabled(true);
+            mGoogleApiClient = new GoogleApiClient.Builder(this)
+                    .addApi(LocationServices.API)
+                    .addConnectionCallbacks(this)
+                    .addOnConnectionFailedListener(this).build();
+            connectClient();
+        }
+    }
 
     protected void connectClient() {
         // Connect the client.
@@ -113,82 +151,82 @@ public class MapDemoActivity extends AppCompatActivity implements
     }
 
     /*
-	 * Called when the Activity is no longer visible.
+     * Called when the Activity is no longer visible.
 	 */
-	@Override
-	protected void onStop() {
-		// Disconnecting the client invalidates it.
+    @Override
+    protected void onStop() {
+        // Disconnecting the client invalidates it.
         if (mGoogleApiClient != null) {
             mGoogleApiClient.disconnect();
         }
-		super.onStop();
-	}
+        super.onStop();
+    }
 
-	/*
-	 * Handle results returned to the FragmentActivity by Google Play services
-	 */
-	@Override
-	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-		// Decide what to do based on the original request code
-		switch (requestCode) {
+    /*
+     * Handle results returned to the FragmentActivity by Google Play services
+     */
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        // Decide what to do based on the original request code
+        switch (requestCode) {
 
-		case CONNECTION_FAILURE_RESOLUTION_REQUEST:
+            case CONNECTION_FAILURE_RESOLUTION_REQUEST:
 			/*
 			 * If the result code is Activity.RESULT_OK, try to connect again
 			 */
-			switch (resultCode) {
-			case Activity.RESULT_OK:
-				mGoogleApiClient.connect();
-				break;
-			}
+                switch (resultCode) {
+                    case Activity.RESULT_OK:
+                        mGoogleApiClient.connect();
+                        break;
+                }
 
-		}
-	}
+        }
+    }
 
-	private boolean isGooglePlayServicesAvailable() {
-		// Check that Google Play services is available
-		int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
-		// If Google Play services is available
-		if (ConnectionResult.SUCCESS == resultCode) {
-			// In debug mode, log the status
-			Log.d("Location Updates", "Google Play services is available.");
-			return true;
-		} else {
-			// Get the error dialog from Google Play services
-			Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this,
-					CONNECTION_FAILURE_RESOLUTION_REQUEST);
-
-			// If Google Play services can provide an error dialog
-			if (errorDialog != null) {
-				// Create a new DialogFragment for the error dialog
-				ErrorDialogFragment errorFragment = new ErrorDialogFragment();
-				errorFragment.setDialog(errorDialog);
-				errorFragment.show(getSupportFragmentManager(), "Location Updates");
-			}
-
-			return false;
-		}
-	}
-
-	/*
-	 * Called by Location Services when the request to connect the client
-	 * finishes successfully. At this point, you can request the current
-	 * location or start periodic updates
-	 */
-	@Override
-	public void onConnected(Bundle dataBundle) {
-		// Display the connection status
-		Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-		if (location != null) {
-			Toast.makeText(this, "GPS location was found!", Toast.LENGTH_SHORT).show();
-			LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
-			CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
-			map.animateCamera(cameraUpdate);
+    private boolean isGooglePlayServicesAvailable() {
+        // Check that Google Play services is available
+        int resultCode = GooglePlayServicesUtil.isGooglePlayServicesAvailable(this);
+        // If Google Play services is available
+        if (ConnectionResult.SUCCESS == resultCode) {
+            // In debug mode, log the status
+            Log.d("Location Updates", "Google Play services is available.");
+            return true;
         } else {
-			Toast.makeText(this, "Current location was null, enable GPS on emulator!", Toast.LENGTH_SHORT).show();
-		}
-		startLocationUpdates();
-	}
+            // Get the error dialog from Google Play services
+            Dialog errorDialog = GooglePlayServicesUtil.getErrorDialog(resultCode, this,
+                    CONNECTION_FAILURE_RESOLUTION_REQUEST);
+
+            // If Google Play services can provide an error dialog
+            if (errorDialog != null) {
+                // Create a new DialogFragment for the error dialog
+                ErrorDialogFragment errorFragment = new ErrorDialogFragment();
+                errorFragment.setDialog(errorDialog);
+                errorFragment.show(getSupportFragmentManager(), "Location Updates");
+            }
+
+            return false;
+        }
+    }
+
+    /*
+     * Called by Location Services when the request to connect the client
+     * finishes successfully. At this point, you can request the current
+     * location or start periodic updates
+     */
+    @Override
+    public void onConnected(Bundle dataBundle) {
+        // Display the connection status
+        Location location = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        if (location != null) {
+            Toast.makeText(this, "GPS location was found!", Toast.LENGTH_SHORT).show();
+            LatLng latLng = new LatLng(location.getLatitude(), location.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(latLng, 17);
+            map.animateCamera(cameraUpdate);
+        } else {
+            Toast.makeText(this, "Current location was null, enable GPS on emulator!", Toast.LENGTH_SHORT).show();
+        }
+        startLocationUpdates();
+    }
 
     protected void startLocationUpdates() {
         mLocationRequest = new LocationRequest();
@@ -221,57 +259,120 @@ public class MapDemoActivity extends AppCompatActivity implements
         }
     }
 
-	/*
-	 * Called by Location Services if the attempt to Location Services fails.
-	 */
-	@Override
-	public void onConnectionFailed(ConnectionResult connectionResult) {
+    /*
+     * Called by Location Services if the attempt to Location Services fails.
+     */
+    @Override
+    public void onConnectionFailed(ConnectionResult connectionResult) {
 		/*
 		 * Google Play services can resolve some errors it detects. If the error
 		 * has a resolution, try sending an Intent to start a Google Play
 		 * services activity that can resolve error.
 		 */
-		if (connectionResult.hasResolution()) {
-			try {
-				// Start an Activity that tries to resolve the error
-				connectionResult.startResolutionForResult(this,
-						CONNECTION_FAILURE_RESOLUTION_REQUEST);
+        if (connectionResult.hasResolution()) {
+            try {
+                // Start an Activity that tries to resolve the error
+                connectionResult.startResolutionForResult(this,
+                        CONNECTION_FAILURE_RESOLUTION_REQUEST);
 				/*
 				 * Thrown if Google Play services canceled the original
 				 * PendingIntent
 				 */
-			} catch (IntentSender.SendIntentException e) {
-				// Log the error
-				e.printStackTrace();
-			}
-		} else {
-			Toast.makeText(getApplicationContext(),
-					"Sorry. Location services not available to you", Toast.LENGTH_LONG).show();
-		}
-	}
+            } catch (IntentSender.SendIntentException e) {
+                // Log the error
+                e.printStackTrace();
+            }
+        } else {
+            Toast.makeText(getApplicationContext(),
+                    "Sorry. Location services not available to you", Toast.LENGTH_LONG).show();
+        }
+    }
 
-	// Define a DialogFragment that displays the error dialog
-	public static class ErrorDialogFragment extends DialogFragment {
+    // Define a DialogFragment that displays the error dialog
+    public static class ErrorDialogFragment extends DialogFragment {
 
-		// Global field to contain the error dialog
-		private Dialog mDialog;
+        // Global field to contain the error dialog
+        private Dialog mDialog;
 
-		// Default constructor. Sets the dialog field to null
-		public ErrorDialogFragment() {
-			super();
-			mDialog = null;
-		}
+        // Default constructor. Sets the dialog field to null
+        public ErrorDialogFragment() {
+            super();
+            mDialog = null;
+        }
 
-		// Set the dialog to display
-		public void setDialog(Dialog dialog) {
-			mDialog = dialog;
-		}
+        // Set the dialog to display
+        public void setDialog(Dialog dialog) {
+            mDialog = dialog;
+        }
 
-		// Return a Dialog to the DialogFragment.
-		@Override
-		public Dialog onCreateDialog(Bundle savedInstanceState) {
-			return mDialog;
-		}
-	}
+        // Return a Dialog to the DialogFragment.
+        @Override
+        public Dialog onCreateDialog(Bundle savedInstanceState) {
+            return mDialog;
+        }
+    }
+
+    class RequestTask extends AsyncTask<String, String, double[]> {
+
+        private static final String TAG = "Debug" ;
+
+        @Override
+        protected double[] doInBackground(String... uri) {
+            double[] responseString = new double[2];
+            try {
+                java.net.URL url = new URL(uri[0]);
+                HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+                if(conn.getResponseCode() == HttpsURLConnection.HTTP_OK)
+                {
+                    InputStream in = new BufferedInputStream(conn.getInputStream());
+                    JSONArray ja = new JSONArray(convertInputStreamToString(in));
+                    for (int i = 0; i < ja.length(); i++) {
+                        JSONObject jo = ja.getJSONObject(i);
+                        latitude = jo.getDouble("latitude");
+                        longitude = jo.getDouble("longitude");
+                        responseString[0] = latitude;
+                        responseString[1] = longitude;
+                        break;
+                    }
+                }
+                else {
+                    String response = "FAILED";
+                    Log.d(TAG, "doInBackground3: " +response);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            Log.d(TAG, "doInBackground4: " +responseString[0]);
+            return responseString;
+
+        }
+
+        @Override
+        protected void onPostExecute(double... result) {
+            super.onPostExecute(result);
+            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(result[0], result[1]), 15));
+//
+            map.addMarker(new MarkerOptions()
+                    .title("11.03.2016, 10:46")
+//                    .icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher))
+                    .anchor(0.0f, 1.0f) // Anchors the marker on the bottom left
+                    .position(new LatLng(result[0], result[1])));
+
+
+        }
+    }
+
+    private static String convertInputStreamToString(InputStream inputStream) throws IOException{
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+        String line = "";
+        String result = "";
+        while ((line = bufferedReader.readLine()) != null) {
+            result += line;
+        }
+        inputStream.close();
+        return result;
+    }
 
 }
